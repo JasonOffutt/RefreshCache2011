0846848a5af89cb449ed9c5753b9b3f5ab1df182
JasonOffutt <jason.offutt@gmail.com>
2011-10-05 14:25:59 -0700

    adding spaces to align comments

--- js-deepdive/this.js
+++ js-deepdive/this.js
@@ -2,25 +2,25 @@ function sayHello(name) {
 	console.log(this + ' says "Hello ' + name + '!"');
 }
 
-sayHello('world');									// This line...
-sayHello.call(window, 'world');						// is functionally equivalent to this line.
+sayHello('world');                                  // This line...
+sayHello.call(window, 'world');                     // is functionally equivalent to this line.
 
-sayHello.call('Jason', 'world');					// Modifying 'this' within the scope of the function being called
+sayHello.call('Jason', 'world');                    // Modifying 'this' within the scope of the function being called
 
 var person = {
 	name: 'Jason Offutt',
-	hello: sayHello,								// Assign our function to be an instance method for person object.
-	toString: function() {							// Overriding the toString() method on our nice little object...
-		return this.name;							// just to be a little bit more useful.
+	hello: sayHello,                                // Assign our function to be an instance method for person object.
+	toString: function() {                          // Overriding the toString() method on our nice little object...
+		return this.name;                           // just to be a little bit more useful.
 	}
 };
 
-person.hello('world');								// This line...
-person.hello.call(person, 'world');					// is functionally equivalent to this line.
+person.hello('world');                              // This line...
+person.hello.call(person, 'world');                 // is functionally equivalent to this line.
 
 var bind = function(func, valueOfThis) {
 	return function() {
-		return func.apply(valueOfThis, arguments);	// arguments is an array-like container for all function params passed in.
+		return func.apply(valueOfThis, arguments);  // arguments is an array-like container for all function params passed in.
 	}
 };
 
