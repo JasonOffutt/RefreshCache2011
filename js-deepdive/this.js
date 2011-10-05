7ec4a85453abfdac861e86b7d64f5230f4e04dc1
JasonOffutt <jason.offutt@gmail.com>
2011-10-05 14:44:23 -0700

    formatifying code examples

--- js-deepdive/this.js
+++ js-deepdive/this.js
@@ -2,21 +2,21 @@ function sayHello(name) {
 	console.log(this + ' says "Hello ' + name + '!"');
 }
 
-sayHello('world');                                  // This line...
-sayHello.call(window, 'world');                     // is functionally equivalent to this line.
+sayHello('world');                                          // This line...
+sayHello.call(window, 'world');                             // is functionally equivalent to this line.
 
-sayHello.call('Jason', 'world');                    // Modifying 'this' within the scope of the function being called
+sayHello.call('Jason', 'world');                            // Modifying 'this' within the scope of the function being called
 
 var person = {
 	name: 'Jason Offutt',
-	hello: sayHello,                                // Assign our function to be an instance method for person object.
-	toString: function() {                          // Overriding the toString() method on our nice little object...
+	hello: sayHello,                                    // Assign our function to be an instance method for person object.
+	toString: function() {                              // Overriding the toString() method on our nice little object...
 		return this.name;                           // just to be a little bit more useful.
 	}
 };
 
-person.hello('world');                              // This line...
-person.hello.call(person, 'world');                 // is functionally equivalent to this line.
+person.hello('world');                                      // This line...
+person.hello.call(person, 'world');                         // is functionally equivalent to this line.
 
 var bind = function(func, valueOfThis) {
 	return function() {
